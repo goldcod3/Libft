4adc89b8c8e2e017c109d992095290074890f95b
goldcod3 <devgomesld@gmail.com>
2022-06-28 03:05:20 +0200

    017 - Correction of ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,13 @@
 /*   By: lgomes-o <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 17:58:06 by lgomes-o          #+#    #+#             */
-/*   Updated: 2022/06/27 18:44:06 by lgomes-o         ###   ########.fr       */
+/*   Updated: 2022/06/28 02:50:17 by lgomes-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
-static size_t	count_words(char *str, char d)
+static size_t	count_words(const char *str, char d)
 {
 	size_t	i;
 	size_t	words;
@@ -22,39 +21,68 @@ static size_t	count_words(char *str, char d)
 	words = 0;
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i) != d)
-		{	
-			while (*(str + i) != d && *(str + i) != '\0')
-				i++;
+		while (*(str + i) == d)
+			i++;
+		if (*(str + i) != '\0')
 			words++;
-		}
-		i++;
+		while (*(str + i) != '\0' && *(str + i) != d)
+			i++;
 	}
 	return (words);
 }
 
-static void	put_words(char **spl, char *str, char d, size_t len)
+static void	put_word(char *word, const char *str, size_t i, size_t size)
 {
-	unsigned int	start;
-	size_t			end;
-	size_t			i;
+	size_t	iw;
 
-	start = 0;
-	i = 0;
-	while (*(str + start) != '\0' && i < len)
+	iw = 0;
+	while (size > 0)
 	{
-		if (*(str + start) != d)
-		{	
-			end = start;
-			while (*(str + (end)) != '\0' && *(str + end) != d)
-				end++;
-			*(spl + i) = ft_substr(str, start, (end - start));
-			start = end;
+		*(word + iw) = *(str + (i - size));
+		iw++;
+		size--;
+	}
+	*(word + iw) = '\0';
+}
+
+static char	**free_spl(char **spl, size_t wk)
+{
+	while (wk > 0)
+	{
+		wk--;
+		free(*(spl + wk));
+	}
+	free(spl);
+	return (NULL);
+}
+
+static char	**allocate_words(char **spl, const char *str, char d, size_t len)
+{
+	size_t		i;
+	size_t		w_len;
+	size_t		w_num;
+
+	i = 0;
+	w_len = 0;
+	w_num = 0;
+	while (w_num < len)
+	{
+		while (*(str + i) != '\0' && *(str + i) == d)
+			i++;
+		while (*(str + i) != '\0' && *(str + i) != d)
+		{
+			w_len++;
 			i++;
 		}
-		else
-			start++;
+		*(spl + w_num) = (char *) malloc((w_len + 1) * sizeof(char));
+		if (*(spl + w_num) == NULL)
+			return (free_spl((char **)spl, w_num));
+		put_word(*(spl + w_num), str, i, w_len);
+		w_len = 0;
+		w_num++;
 	}
+	*(spl + w_num) = NULL;
+	return (spl);
 }
 
 char	**ft_split(const char *s, char c)
@@ -62,12 +90,11 @@ char	**ft_split(const char *s, char c)
 	char	**split;
 	size_t	w;
 
-	if (s != NULL && c != '\0')
-	{	
-		w = count_words((char *)s, c);
-		split = (char **) ft_calloc(w + 1, sizeof(char *));
-		put_words(split, (char *)s, c, w);
-		return (split);
-	}
-	return (NULL);
+	if (s == NULL)
+		return (NULL);
+	w = count_words(s, c);
+	split = (char **) malloc((w + 1) * sizeof(char *));
+	if (split == NULL)
+		return (NULL);
+	return (allocate_words(split, s, c, w));
 }
